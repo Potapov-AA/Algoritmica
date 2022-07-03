9cf6d82ed640a419197dd88e9f462fdbd21fd595
Alexandr Potapov <San_sanih99@mail.ru>
2022-07-03 12:43:05 +0300

    added new calls

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from search import BinarySearch
 from sort import SelectionSort
+from factorial import Factorial
 
 
 if __name__ == '__main__':
@@ -7,3 +8,5 @@ if __name__ == '__main__':
     _list_for_find = SelectionSort(_list_for_sort).selectionSort()
     print(_list_for_find)
     print(BinarySearch(_list_for_find).binary_search(5))
+    print(Factorial().factorial(5))
+
