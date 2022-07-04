2e9763c77bf8611bd1052aa7c6ffb48215c505d9
Alexandr Potapov <San_sanih99@mail.ru>
2022-07-04 09:43:09 +0300

    connect files

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-from search import BinarySearch
-from sort import SelectionSort
+from search import BinarySearch, BFS, Dijkstra
+from sort import SelectionSort, QuickSort
 from factorial import Factorial
 
-
 if __name__ == '__main__':
-    _list_for_sort = [2, 5, 7, 1, 3, 4, 6, 11]
-    _list_for_find = SelectionSort(_list_for_sort).selectionSort()
-    print(_list_for_find)
-    print(BinarySearch(_list_for_find).binary_search(5))
-    print(Factorial().factorial(5))
+
+    pass
 
